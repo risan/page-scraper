39863bdd95880cccbfa2cb92422c7ef96c39199b
risan <risanbagja@gmail.com>
2018-11-07 17:03:32 +0100

    test: Remove unit tests update integration test

--- __tests__/integration.js
+++ __tests__/integration.js
@@ -1,17 +1,7 @@
-const PageScraper = require('../src');
-
-let scraper = null;
-
-beforeEach(() => {
-  scraper = new PageScraper();
-});
-
-afterEach(async () => {
-  await scraper.releaseResources();
-});
+const scrape = require('../src');
 
 test('it can scrape a page', async () => {
-  const $ = await scraper.open('https://example.com');
+  const $ = await scrape('https://example.com');
 
   expect($('title').text().toLowerCase()).toContain('example domain');
   expect($('h1').text().toLowerCase()).toContain('example domain');
@@ -23,10 +13,22 @@ test('it throws an error on http exception', async () => {
   expect.assertions(3);
 
   try {
-    await scraper.open('https://httpbin.org/status/400');
+    await scrape('https://httpbin.org/status/400');
   } catch (error) {
-    expect(error.status).toBe(400);
+    expect(error.response.statusCode).toBe(400);
     expect(error.message.toLowerCase()).toContain('bad request');
     expect(error).toHaveProperty('$');
+  }
+});
+
+test('it throws an error if the page is not an HTML', async () => {
+  expect.assertions(3);
+
+  try {
+    await scrape('https://httpbin.org/json');
+  } catch (error) {
+    expect(error.response.statusCode).toBe(200);
+    expect(error.message.toLowerCase()).toContain('page is not an html');
+    expect(error.$).toBeUndefined();
   }
 });
