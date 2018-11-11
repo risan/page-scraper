c2fd88a2645d730e03108ca723fe204ea84e3c38
risan <risanbagja@gmail.com>
2018-11-11 17:00:06 +0100

    test: Mock got module

---
+++ __tests__/index.js
@@ -0,0 +1,82 @@
+/* global test:false, expect:false */
+const got = require("got");
+const scrape = require("../src");
+
+jest.mock("got");
+
+const body = `
+  <html>
+    <title>foo</title>
+    <body>
+      <h1>foo bar</h1>
+      <a href="http://example.com">foo</a>
+    </body>
+  </html>
+`;
+
+test("it can scrape a page", async () => {
+  got.mockResolvedValue({
+    headers: {
+      "content-type": "text/html"
+    },
+    body
+  });
+
+  const $ = await scrape("http://example.com");
+
+  expect($("title").text()).toBe("foo");
+
+  expect($("h1").text()).toBe("foo bar");
+
+  expect($("a").attr("href")).toBe("http://example.com");
+});
+
+test("it throws an error on http exception", async () => {
+  const error = new Error();
+
+  error.response = {
+    statusCode: 400,
+    statusMessage: "bad request",
+    headers: {
+      "content-type": "text/html"
+    },
+    body
+  };
+
+  got.mockRejectedValue(error);
+
+  expect.assertions(4);
+
+  try {
+    await scrape("http://example.com");
+  } catch (error) {
+    expect(error.response.statusCode).toBe(400);
+
+    expect(error.message).toBe("bad request");
+
+    expect(error).toHaveProperty("$");
+
+    expect(error.$("h1").text()).toBe("foo bar");
+  }
+});
+
+test("it throws an error if the page is not an HTML", async () => {
+  got.mockResolvedValue({
+    statusCode: 200,
+    headers: {
+      "content-type": "application/json"
+    }
+  });
+
+  expect.assertions(3);
+
+  try {
+    await scrape("http://example.com");
+  } catch (error) {
+    expect(error.response.statusCode).toBe(200);
+
+    expect(error.message.toLowerCase()).toContain("page is not an html");
+
+    expect(error.$).toBeUndefined();
+  }
+});
